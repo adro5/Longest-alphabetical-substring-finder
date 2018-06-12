3a8004bfc3c1787044f00144f17aea1c66bc443b
Adam Robinson <adro5@users.noreply.github.com>
2018-06-12 01:05:21 -0400

    Changed type of loop

--- substring.py
+++ substring.py
@@ -5,10 +5,9 @@ Created on Sun Jun 10 19:45:19 2018
 @author: Adam Robinson
 """
 s = str(input("Enter a string of letters: "))
-index = 0
 count = 1 # first letter in string is the current longest alpha string
 result = s[0]
-while index <= (len(s) - 1):
+for index in range(0, len(s)):
     # first cond prevents out of bounds index
     if index < (len(s) - 1) and s[index] <= s[index + 1]:
         count += 1 # if the next letter is greater (alphabetically) than the current index, add to count
@@ -18,5 +17,4 @@ while index <= (len(s) - 1):
         else: #if new string is bigger...
             result = s[index - count + 1: index + 1] # the new substring is our new longest
             count = 1 # reset counter to keep looking      
-    index += 1
 print("Longest substring in alphabetical order is: " + result)
